d9129a5dbe6c327aed048a22203dc64bbaa5a747
372572571 <372572571@qq.com>
2018-02-01 17:28:03 +0800

    Add files via upload

---
+++ controller/indexcontroller.go
@@ -0,0 +1,15 @@
+package controller
+
+import (
+	"webapp/app"
+)
+
+type IndexController struct {
+	app.App
+}
+
+func (i *IndexController) Index() {
+	i.Data["name"] = "A brick"
+	i.Data["email"] = "372572571@qq.com"
+	i.Echo("view/index.html")
+}
---
+++ middlewarefuncs/middlewarefuncs.go
@@ -0,0 +1,10 @@
+package middlewarefuncs
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Index_middleware(w http.ResponseWriter,r *http.Request){
+	fmt.Println("middleware index")
+}
